a49bea12107d1364429ea659405cfd538dcd4278
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-02-18 00:32:42 +0400

    Tabs Display First Job

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { FaAngleDoubleRight } from "react-icons/fa";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tabs-project";
@@ -27,7 +27,31 @@ function App() {
     );
   }
 
-  return <h2>jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center"></div>
+      {/* btn container */}
+      {/* job info */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{duty}</p>
+            </div>
+          );
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
